3e3654929818b145f7c51ab6c4fc91f194dbecb9
liezl76 <lzel_76@yahoo.com>
2025-01-25 11:44:01 +0800

    edit swagger.json and productRoutes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const productsController = require('../controllers/productsController'); // Ensure this path is correct
-const { validateUser, validateProduct } = require('../helpers/validate');
+const { validateProduct } = require('../helpers/validate');
 const validate = require('../middleware/middleware');
 
 // Routes for products operations
 router.get('/', productsController.getAll); // Make sure this function exists in productsController
-router.get('/:id', validateUser, validate, productsController.getSingle); 
+router.get('/:id', validate, productsController.getSingle); 
 router.post('/', validateProduct, validate, productsController.createProducts);
-router.put('/:id', [validateUser, validateProduct], validate, productsController.updateProducts);
-router.delete('/:id', validateUser, validate, productsController.deleteProducts);
+router.put('/:id', [validateProduct], validate, productsController.updateProducts);
+router.delete('/:id', validate, productsController.deleteProducts);
 
 module.exports = router;
